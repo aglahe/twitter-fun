993758b84a5b4faad26a1e3565facd3fa8d94932
Aaron Glahe <aaron.glahe@gmail.com>
2016-03-29 10:32:37 -0400

    Simple Consumer, and also added more filter list

--- kafka-consumer/src/main/java/com/anavationllc/twitter/TwitterConsumer.java
+++ kafka-consumer/src/main/java/com/anavationllc/twitter/TwitterConsumer.java
@@ -1,24 +1,82 @@
 package com.anavationllc.twitter;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.errors.WakeupException;
+import org.apache.log4j.Logger;
 
+import java.util.Collections;
 import java.util.Properties;
 
-/**
- * Created by aarong on 3/22/16.
- */
 public class TwitterConsumer
 {
-    private KafkaConsumer consumer;
+    private static final Logger log = Logger.getLogger(TwitterConsumer.class);
+
+    private boolean done = false;
+    private KafkaConsumer<String, JsonNode> kafkaConsumer;
 
     public TwitterConsumer()
     {
         Properties props = new Properties();
-        props.put("bootstrap.servers", "192.168.99.100");
+        props.put("bootstrap.servers", "192.168.99.100:9092");
         props.put("group.id", "aaron-tester");
-        props.put("key.deseralizer", "StringDeserializer.class.getName()");
-        props.put("value.deseralizer", "StringDeserializer.class.getName()");
-        KafkaConsumer<String, String> consumer = new KafkaConsumer(props);
+        props.put("client.id", "intellij");
+        props.put("auto.offset.reset","earliest");
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.connect.json.JsonDeserializer");
 
+        this.kafkaConsumer = new KafkaConsumer(props);
+    }
+
+    public KafkaConsumer getKafkaConsuner() {
+        return this.kafkaConsumer;
+    }
+
+    public void process() {
+        try{
+            while (!done) {
+                ConsumerRecords<String, JsonNode> records = this.kafkaConsumer.poll(1000);
+                log.info("# of records:" + records.count());
+                for (ConsumerRecord<String, JsonNode> record : records) {
+                    log.info("\t" + record.toString());
+                    log.info("\t" + record.value());
+                }
+            }
+        } catch (WakeupException e) {
+            log.warn(e);
+        } finally {
+            kafkaConsumer.close();
+            log.info("Closed Kafka Consumer");
+        }
+    }
+
+    private void init()
+    {
+        log.info("Subscribing...");
+        this.kafkaConsumer.subscribe(Collections.singletonList("twitter"));
+        log.info("done.");
+    }
+
+    public static void main(String[] args) {
+        final TwitterConsumer twitterConsumer = new TwitterConsumer();
+
+        // Add Shutdown hook
+        final Thread mainThread = Thread.currentThread();
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            public void run() {
+                log.info("Exiting..");
+                twitterConsumer.getKafkaConsuner().wakeup();
+                try {
+                    mainThread.join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        twitterConsumer.init();
+        twitterConsumer.process();
     }
 }
--- producer.py
+++ producer.py
@@ -1,19 +1,19 @@
 import json
 
-import os
 import yaml
 from birdy.twitter import StreamClient
 from kafka import KafkaProducer
 
 TWITTER_AUTH_FILE = 'conf/twitter.yml'
-KAFKA_CONF_FILE = 'conf/kafka.yml'
 
+
+KAFKA_CONF_FILE = 'conf/kafka.yml'
 
 def main():
     #Twitter Setup
     twitter_tokens = yaml.safe_load(open(TWITTER_AUTH_FILE))
     client = StreamClient(twitter_tokens['consumer_key'], twitter_tokens['consumer_secret'],twitter_tokens['access_token'], twitter_tokens['access_secret'])
-    resource = client.stream.statuses.filter.post(track='java')
+    resource = client.stream.statuses.filter.post(track='java,apache,linux,hadoop')
 
     # Kafka Setup
     kafka_conf = yaml.safe_load(open(KAFKA_CONF_FILE))
